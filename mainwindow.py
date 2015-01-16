12f2abc75888bf69c9de5362a3d36df612af4756
Krop <andrekropes@yandex.ru>
2015-01-16 15:52:03 +0200

    cleaning

--- mainwindow.py
+++ mainwindow.py
@@ -10,7 +10,6 @@ window.show()
 
 # [Add widgets to the widget]
 
-# Create some widgets (these won't appear immediately):
 songtags = QLabel("PSY - GANGNAM STYLE")
 playbut = QPushButton(">")
 nextbut = QPushButton(">>")
@@ -28,18 +27,7 @@ mainlayout.addWidget(songslide)
 mainlayout.addWidget(playlist)
 
 
-# Put the widgets in a layout (now they start to appear):
-
-
-# [Resizing the window]
-
-# Let's resize the window:
 window.resize(300, 500)
 
-# The widgets are managed by the layout...
-#window.resize(320, 180)
 
-# [Run the application]
-
-# Start the event loop...
 sys.exit(app.exec_())
